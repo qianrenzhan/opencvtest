0c437b0e0e29803cfce91578fc28ec1eabd3f482
qianrenzhan <ustbgwj@gmail.com>
2018-12-11 14:28:09 +0800

    删除了main.cpp中的注释

--- main.cpp
+++ main.cpp
@@ -3,12 +3,6 @@
 //#include "function.h"
 using namespace std;
 
-/**
- * [main description]
- * @param  argc [description]
- * @param  argv [description]
- * @return      [description]
- */
 int main(int argc, char const *argv[]) {
   /* code */
   return 0;
